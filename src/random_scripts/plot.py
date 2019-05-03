38f092f1ebec4a469523d4ee3bdbfdf25919fb61
Vivek <vivek.7266@gmail.com>
2019-05-03 16:06:40 -0400

    plots added

--- src/random_scripts/plot.py
+++ src/random_scripts/plot.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-f = open("tsn_training.log","r")
+f = open("../training_logs/tsn_training.log","r")
 lines = f.readlines()
 df = list()
 for line in lines[1:]:
@@ -11,16 +11,31 @@ for line in lines[1:]:
 df = pd.DataFrame(df,columns = lines[0].strip().split(','))
 
 text = "Learning Rates:\n Epochs 0-44: 1e-3\n Epochs 45-200:2e-3"
+#text = "Learning rate: 1e-3"
 
 fig,ax = plt.subplots()
-plt.plot(df['epoch'], df['acc'], color = 'b', label = 'Training Accuracy')
-plt.plot(df['epoch'], df['val_acc'], color = 'r', label = 'Validation Accuracy')
+
+plt.plot(df['epoch'], df['loss'], color = 'b', label = 'Training Loss')
+plt.plot(df['epoch'], df['val_loss'], color = 'r', label = 'Validation Loss')
+
+#plt.plot(df['epoch'], df['acc'], color = 'b', label = 'Training Accuracy')
+#plt.plot(df['epoch'], df['val_acc'], color = 'r', label = 'Validation Accuracy')
+
 plt.legend()
-plt.ylabel("Accuracy")
+
+plt.ylabel("Loss")
+#plt.ylabel("Accuracy")
+
 plt.xlabel("Epochs")
-plt.title("TSN Performance")
+
+plt.title("TSN Loss Curve")
+#plt.title("I3D Frame Accuracy Curve")
+
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-ax.text(0.5, 0.3, text, transform=ax.transAxes, fontsize=14,
+ax.text(0.5, 0.5, text, transform=ax.transAxes, fontsize=14,
         verticalalignment='top', bbox=props)
-fig.savefig("tsn_perf_acc.png")
+
+fig.savefig("../results/tsn_perf_loss.png")
+#fig.savefig("i3d_frame_perf_acc.png")
+
 plt.close()
