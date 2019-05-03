34c968bb1cd4d53cd64faa79c8d07d1b22893f9b
rvsingh31 <ravinder.singh3196@gmail.com>
2019-05-03 18:50:20 -0400

    added comments

--- src/models/TSN/pretrained_models.py
+++ src/models/TSN/pretrained_models.py
@@ -13,31 +13,28 @@ import os
 # os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
 
-
-def Network(i1,i2,i3):
-    bm1,fm1 = inception(i1)
-    bm2,fm2 = inception(i2)
-    bm3,fm3 = inception(i3)
-    out = average([fm1.output, fm2.output, fm3.output])
-
-    return out
-
-
 def TSN():
-
+    #Contains 3 spatial ConvNets and 3 Temporal Convnets
+    
     #Spatial Layers
     with K.name_scope('SpatialLayer1'):
+        #Input Layer 1
         i1 = Input(shape = (224,224,3), name = "input_1")
+        #Using InceptionResnetV2 of keras with pretrained weights
         bm1 = InceptionResNetV2(input_tensor = i1, weights='imagenet', include_top=False)
         x = bm1.output
+        #using the output layer of pretrained model for transfer learning
         x = GlobalAveragePooling2D()(x)
         x = Dropout(rate = 0.5)(x)
+        #Custom classifier for our application
         sp1 = Dense(101, activation='softmax')(x)
+        #Freeze layers for training
         for layer in bm1.layers[:-1]:
             layer.trainable = False
             if isinstance(layer, keras.layers.normalization.BatchNormalization):
                 layer.trainable = False
 
+    #Next 2 are 2 spatial convnets
     with K.name_scope('SpatialLayer2'):
         i2 = Input(shape = (224,224,3), name = "input_2")
         bm2 = InceptionResNetV2(input_tensor = i2, weights='imagenet', include_top=False)
@@ -63,9 +60,11 @@ def TSN():
             if isinstance(layer, keras.layers.normalization.BatchNormalization):
                 layer.trainable = False
 
+    #Taking a consensus of all spatial layers
     out1 = average([sp1, sp2, sp3])
 
 
+    #Similar to spatial networks, these will work on optical flows
     #Temporal Layers
     with K.name_scope('TemporalLayer1'):
         i4 = Input(shape = (224,224,3), name = "input_4")
@@ -105,15 +104,15 @@ def TSN():
             if isinstance(layer, keras.layers.normalization.BatchNormalization):
                 layer.trainable = False
 
-
+    #Consensus of temporal networks
     out2 = average([sp4, sp5, sp6])
 
-    #Combined
+    #Combined consensus of temporal and spatial
     res = average([out1, out2], name = 'output')
     model = Model(inputs = [i1,i2,i3,i4,i5,i6], outputs =[res])
 
     return model
 
-model = TSN()
+#model = TSN()
 # plot_model(model, to_file='pretrained_model_iv2.png', show_shapes = True)
-print(model.summary()) 
+#print(model.summary()) 
